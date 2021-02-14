230e4103dedc87ad60d9753d8a43aba644285d52
sebastianbugal <49225786+sebastianbugal@users.noreply.github.com>
2021-02-13 17:27:14 -0800

    tokens done

--- main.py
+++ main.py
@@ -1,12 +1,51 @@
 import nltk
 import re
 import pandas as pd
+import numpy as np
+
+def main():
+    pairs = process_data()
+    find_tokens(pairs)
+
+
+
+
+def find_tokens(pairs):
+    input_arr, output_arr = [], []
+    in_token = set()
+    out_token = set()
+
+    for i in pairs:
+        input, output = i['in'], i['out']
+        input = input.lower()
+        output = output.lower()
+        input_arr.append(input)
+        output = " ".join(re.findall(r"[\w']+|[^\s\w]", output))
+        output = '<START> ' + output + ' <END>'
+        output_arr.append(output)
+
+        for tok in re.findall(r"[\w']+|[^\s\w]", input):
+            if tok not in in_token:
+                in_token.add(tok)
+        for tok in output.split():
+            if tok not in out_token:
+                out_token.add(tok)
+
+    in_token = sorted(list(in_token))
+    out_token = sorted(list(out_token))
+    in_token_len = len(in_token)
+    out_token_len = len(out_token)
+
+    return {'in_token': in_token, 'out_token': out_token, 'input_arr': input_arr, 'output_arr': output_arr, 'in_token_len': in_token_len, 'out_token_len': out_token_len}
+
 
 def print_dataset(fileName, n=100):
     with open(fileName, 'rb') as datafile:
         lines = datafile.readlines()
         for line in lines[:n]:
             print(line)
+
+
 def load_lines(fileName, fields = ["lineID", "characterID", "movieID", "character", "text"]):
     lines = {}
     with open(fileName, 'r', encoding='iso-8859-1') as f:
@@ -18,32 +57,13 @@ def load_lines(fileName, fields = ["lineID", "characterID", "movieID", "characte
                 lineObj[field] = values[i]
             lines[lineObj['lineID']] = lineObj
     return lines
-# print_dataset('cornell movie-dialogs corpus/movie_lines.txt')
-dataset = load_lines('cornell movie-dialogs corpus/movie_lines.txt')
-# df = pd.DataFrame()
-# data = pd.DataFrame(dataset).T
-
-men = ['u0', 'u7133', 'u667', 'u695', 'u6937', 'u861', 'u7366', 'u6912', 'u1755', 'u3784', 'u2010', 'u3819', '7384', 'u2495', 'u6378', 'u2511']
-# df = df.append(data[(data['movieID'] == 'm0') & ((data['characterID'] == 'u0') | (data['characterID'] == 'u2'))])
-# df = df.append(data[(data['movieID'] == 'm479') & ((data['characterID'] == 'u7133') | (data['characterID'] == 'u7142'))])
-# df = df.append(data[(data['movieID'] == 'm42') & ((data['characterID'] == 'u667') | (data['characterID'] == 'u664'))])
-# df = df.append(data[(data['movieID'] == 'm44') & ((data['characterID'] == 'u695') | (data['characterID'] == 'u706'))])
-# df = df.append(data[(data['movieID'] == 'm464') & ((data['characterID'] == 'u6937') | (data['characterID'] == 'u6940'))])
-# df = df.append(data[(data['movieID'] == 'm55') & ((data['characterID'] == 'u861') | (data['characterID'] == 'u859'))])
-# df = df.append(data[(data['movieID'] == 'm498') & ((data['characterID'] == 'u7366 ') | (data['characterID'] == 'u7367'))])
-# df = df.append(data[(data['movieID'] == 'm462') & ((data['characterID'] == 'u6912 ') | (data['characterID'] == 'u6896'))])
-# df = df.append(data[(data['movieID'] == 'm116') & ((data['characterID'] == 'u1755') | (data['characterID'] == 'u1758'))])
-# df = df.append(data[(data['movieID'] == 'm249') & ((data['characterID'] == 'u3784') | (data['characterID'] == 'u3777'))])
-# df = df.append(data[(data['movieID'] == 'm130') & ((data['characterID'] == 'u2010') | (data['characterID'] == 'u2004'))])
-# df = df.append(data[(data['movieID'] == 'm252') & ((data['characterID'] == 'u3819') | (data['characterID'] == 'u3820'))])
-# df = df.append(data[(data['movieID'] == 'm499') & ((data['characterID'] == 'u7384') | (data['characterID'] == 'u7372'))])
-# df = df.append(data[(data['movieID'] == 'm161') & ((data['characterID'] == 'u2495') | (data['characterID'] == 'u2485'))])
-# df = df.append(data[(data['movieID'] == 'm425') & ((data['characterID'] == 'u6378') | (data['characterID'] == 'u6379'))])
-# df = df.append(data[(data['movieID'] == 'm163') & ((data['characterID'] == 'u2511') |(data['characterID'] == 'u2526') | (data['characterID'] == 'u2521'))])
 
 
 def process_data():
     pairs = []
+    men = ['u0', 'u7133', 'u667', 'u695', 'u6937', 'u861', 'u7366', 'u6912', 'u1755', 'u3784', 'u2010', 'u3819', '7384',
+           'u2495', 'u6378', 'u2511']
+
     movieID_array = ['m0', 'm479', 'm42', 'm43', 'm464', 'm55',
                      'm498', 'm462', 'm116', 'm249', 'm130',
                      'm252', 'm499', 'm161', 'm425', 'm163']
@@ -63,6 +83,82 @@ def process_data():
                     man = row
             elif man:
                 if man[1]['lineID']-1 == row[1]['lineID'] or man[1]['lineID']+1 == row[1]['lineID']:
-                    pairs.append({'in':man[1]['text'],'out':row[1]['text']})
+                    pairs.append({'in': man[1]['text'],'out': row[1]['text']})
+    return pairs
 
-process_data()
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print_dataset('cornell movie-dialogs corpus/movie_lines.txt')
+dataset = load_lines('cornell movie-dialogs corpus/movie_lines.txt')
+# df = pd.DataFrame()
+# data = pd.DataFrame(dataset).T
+
+# df = df.append(data[(data['movieID'] == 'm0') & ((data['characterID'] == 'u0') | (data['characterID'] == 'u2'))])
+# df = df.append(data[(data['movieID'] == 'm479') & ((data['characterID'] == 'u7133') | (data['characterID'] == 'u7142'))])
+# df = df.append(data[(data['movieID'] == 'm42') & ((data['characterID'] == 'u667') | (data['characterID'] == 'u664'))])
+# df = df.append(data[(data['movieID'] == 'm44') & ((data['characterID'] == 'u695') | (data['characterID'] == 'u706'))])
+# df = df.append(data[(data['movieID'] == 'm464') & ((data['characterID'] == 'u6937') | (data['characterID'] == 'u6940'))])
+# df = df.append(data[(data['movieID'] == 'm55') & ((data['characterID'] == 'u861') | (data['characterID'] == 'u859'))])
+# df = df.append(data[(data['movieID'] == 'm498') & ((data['characterID'] == 'u7366 ') | (data['characterID'] == 'u7367'))])
+# df = df.append(data[(data['movieID'] == 'm462') & ((data['characterID'] == 'u6912 ') | (data['characterID'] == 'u6896'))])
+# df = df.append(data[(data['movieID'] == 'm116') & ((data['characterID'] == 'u1755') | (data['characterID'] == 'u1758'))])
+# df = df.append(data[(data['movieID'] == 'm249') & ((data['characterID'] == 'u3784') | (data['characterID'] == 'u3777'))])
+# df = df.append(data[(data['movieID'] == 'm130') & ((data['characterID'] == 'u2010') | (data['characterID'] == 'u2004'))])
+# df = df.append(data[(data['movieID'] == 'm252') & ((data['characterID'] == 'u3819') | (data['characterID'] == 'u3820'))])
+# df = df.append(data[(data['movieID'] == 'm499') & ((data['characterID'] == 'u7384') | (data['characterID'] == 'u7372'))])
+# df = df.append(data[(data['movieID'] == 'm161') & ((data['characterID'] == 'u2495') | (data['characterID'] == 'u2485'))])
+# df = df.append(data[(data['movieID'] == 'm425') & ((data['characterID'] == 'u6378') | (data['characterID'] == 'u6379'))])
+# df = df.append(data[(data['movieID'] == 'm163') & ((data['characterID'] == 'u2511') |(data['characterID'] == 'u2526') | (data['characterID'] == 'u2521'))])
+
+
